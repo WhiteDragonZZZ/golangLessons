d14d636d88636c897d4f5afbe81c8d46a354d002
Zhass <zhass.akimbayev@gmail.com>
2022-09-07 01:55:27 +0600

    newBranch commit

--- task4/main.go
+++ task4/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	print("Hello")
+	var B int
+	pntr := &B
+	B = 54
+	*pntr = 66
+	fmt.Println(*pntr)
+
+	var value float64
+	fmt.Scan(&value)
+	R := &value
+	var O float64
+	O = math.Pow(*R, 2) * 3.14
+	fmt.Println(O)
 }
